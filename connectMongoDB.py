dac5b2c5588392deecf21b223407777ef7ee4263
Manuel <Manwelanza@gmail.com>
2017-11-21 16:01:08 +0100

    Very dirty code

--- connectMongoDB.py
+++ connectMongoDB.py
@@ -9,6 +9,7 @@ CONST_QUOTE = "quoted_status"
 
 cliente = MongoClient('localhost', 27017)
 db = cliente['test1']
+clearTweet = client['test1'].clearTweet
 
 def getTweet (id_str):
     cursor = (db.tweets
@@ -52,6 +53,26 @@ def getReplys ():
                 )
             )
 
+def processRT(tweet):
+    originalTweet = getOriginalTweet(tweet[CONST_RT])
+
+    if clearTweet.find({"tweetId": originalTweet["id_str"]}).count() == 0:
+        insertTweet(originalTweet)
+
+    
+    
+def getOriginalTweet(tweet):
+    if CONST_QUOTE in tweet:
+        return getOriginalTweet(tweet[CONST_QUOTE])
+    else:
+        return tweet
+
+def processQuote(tweet, isRT):
+    pass
+
+def insertTweet (tweet):
+    pass
+
 """
 tweet = getTweet('917946195410128897')
 print(sys.getdefaultencoding())
@@ -68,10 +89,15 @@ originals = getOriginals()
 replys = getReplys()
 
 for t in tweets:
+    isRT = False
     if CONST_RT in t:
+        isRT = True
         RTcount += 1
-    elif CONST_QUOTE in t:
+        processRT(t)
+    
+    if CONST_QUOTE in t:
         quoteCount += 1
+        processQuote(t, isRT)
 
 print ("{0} RTs of {1} tweets".format(RTcount, tweets.count()))
 print ("{0} quotes of {1} tweets".format(quoteCount, tweets.count()))
