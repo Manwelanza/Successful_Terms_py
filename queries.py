abd4f2b66ea6060d85e0ae5908afd1c8de4070a5
Manuel <Manwelanza@gmail.com>
2017-11-23 18:06:33 +0100

    Cleaning and restructure code

---
+++ queries.py
@@ -0,0 +1,24 @@
+CONST_RT = "retweeted_status"
+CONST_QUOTE = "quoted_status"
+
+def getRTs ():
+    return {"retweeted_status": { "$exists" : True}}
+
+def getQuotes ():
+    return {"quoted_status": { "$exists" : True}}
+
+def getNormalTweets():
+    return ({
+            "$and": [
+                        {"quoted_status": { "$exists" : False}},
+                        {"retweeted_status": { "$exists" : False}},
+                        {"in_reply_to_status_id_str": None}
+                    ]
+            })
+
+def getReplys ():
+    return {"in_reply_to_status_id_str": {"$ne": None}}
+
+def getTweet (idLabel, idValue):
+    return {idLabel: idValue}
+
