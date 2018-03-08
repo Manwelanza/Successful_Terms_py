8c1fba43ecdc470333945976a3d8f0ebd416e661
Manuel <Manwelanza@gmail.com>
2018-03-08 20:24:12 +0100

    Playing with dataFrames and linearRegression

--- testingSpark.py
+++ testingSpark.py
@@ -1,20 +1,51 @@
 from pyspark.ml.regression import LinearRegression
 from pyspark.context import SparkContext
 from pyspark.sql.session import SparkSession
+from pyspark.ml.linalg import Vectors
 
-#sc = SparkContext('local')
-#spark = SparkSession(sc)
-spark = SparkSession.builder.master("local").getOrCreate()
+sc = SparkContext('local')
+spark = SparkSession(sc)
+#spark = SparkSession.builder.master("local").getOrCreate()
 
 # Load training data
-training = spark.read.format("libsvm")\
-    .load("sparkData/sample_linear_regression_data.txt")
+"""training = spark.read.format("libsvm")\
+    .load("sparkData/sample_regression_data.txt")"""
+
+
+
+training = spark.createDataFrame(
+    [
+        (0, Vectors.dense([1])),
+        (0, Vectors.dense([2])),
+        (1, Vectors.dense([3])),
+        (1, Vectors.dense([4]))
+    ],
+    ["label", "features"]
+)
 
 lr = LinearRegression(maxIter=10, regParam=0.3, elasticNetParam=0.8)
 
 # Fit the model
 lrModel = lr.fit(training)
 
+test0 = spark.createDataFrame(
+    [
+        (0, Vectors.dense([-1])),
+        (0, Vectors.dense([1])),
+        (1, Vectors.dense([6])),
+        (1, Vectors.dense([4]))
+    ],
+    ["label", "features"]
+)
+predictions0 = lrModel.transform(test0)
+print(predictions0.collect)
+result = predictions0.select("features", "label", "prediction") \
+    .collect()
+
+for row in result:
+    print("features=%s, label=%s -> prediction=%s"
+          % (row.features, row.label, row.prediction))
+"""
 # Print the coefficients and intercept for linear regression
 print("Coefficients: %s" % str(lrModel.coefficients))
 print("Intercept: %s" % str(lrModel.intercept))
@@ -25,4 +56,5 @@ print("numIterations: %d" % trainingSummary.totalIterations)
 print("objectiveHistory: %s" % str(trainingSummary.objectiveHistory))
 trainingSummary.residuals.show()
 print("RMSE: %f" % trainingSummary.rootMeanSquaredError)
-print("r2: %f" % trainingSummary.r2)
+print("r2: %f" % trainingSummary.r2)"
+"""
